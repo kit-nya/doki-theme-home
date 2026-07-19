a18dfb69df5a8f9ba899330b5b267062746bf880
Kittu <kittu@kittu.dev>
2026-07-19 13:04:18 +0900

    change

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,10 @@
 import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
-import routes from './routes.js';
+import fs from 'fs';
+
+const routes = fs.existsSync('./routes.js')
+	? (await import('./routes.js')).default
+	: [];
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
